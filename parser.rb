ebdec35e00a0a101844b9a3dffb1dfd793f2b77f
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-08-31 18:05:18 +1200

    Add pdf parser

---
+++ parser.rb
@@ -0,0 +1,29 @@
+require 'pdf-reader'
+
+File.open('example.pdf', 'rb') do |io|
+  reader = PDF::Reader.new(io)
+  pages = []
+
+  # Parsing PDF
+  reader.pages.each do |p|
+    rows = []
+    # Separating a whole text
+    t = p.text.split("\n")
+
+    t.each do |s|
+      # Formatting
+      ary = s.split("\s\s")
+      ary.delete_if { |str| str.nil? || str.empty? }
+      ary.each(&:strip!)
+      next if ary.empty?
+
+      rows << ary
+    end
+    pages << rows
+  end
+
+  # Showing parsed data
+  pages.each do |page|
+    page.each { |rows| p rows }
+  end
+end
